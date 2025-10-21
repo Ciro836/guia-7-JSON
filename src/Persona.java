4c5ca75a66449aaba248f14b213493dc837871b9
Ciro Schot <cirschot@gmail.com>
2025-10-21 09:09:07 -0300

    Agregue las ibrerias JSON

--- src/Persona.java
+++ src/Persona.java
@@ -5,4 +5,52 @@ public class Persona
     private int dni;
     private char sexo;
 
+    public Persona(String nombre, int edad, int dni, char sexo)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.sexo = sexo;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public int getEdad()
+    {
+        return edad;
+    }
+
+    public void setEdad(int edad)
+    {
+        this.edad = edad;
+    }
+
+    public int getDni()
+    {
+        return dni;
+    }
+
+    public void setDni(int dni)
+    {
+        this.dni = dni;
+    }
+
+    public char getSexo()
+    {
+        return sexo;
+    }
+
+    public void setSexo(char sexo)
+    {
+        this.sexo = sexo;
+    }
+
 }
